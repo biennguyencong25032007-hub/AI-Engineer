5c0314a6e613ad0202b7c895cb9e9f8b6e656571
biennguyencong25032007-hub <biennguyencong25032007@gmail.com>
2026-04-09 21:16:57 +0700

    .

---
+++ Platform Python/student_manager/main.py
@@ -0,0 +1,8 @@
+def show_menu():
+    print("\n===== STUDENT MANAGER =====")
+    print("1. Add student")
+    print("2. Show all")
+    print("3. Update student")
+    print("4. Delete student")
+    print("5. Statistics")
+    print("6. Exit")
